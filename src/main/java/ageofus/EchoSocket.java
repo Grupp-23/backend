fceb20bfdb8e4957190f438ed18c047382905b95
jankzzz <fredriklabani@gmail.com>
2023-04-24 17:19:00 +0200

    MatchThread

--- src/main/java/ageofus/EchoSocket.java
+++ src/main/java/ageofus/EchoSocket.java
@@ -6,22 +6,30 @@ import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.IOException;
+import java.util.*;
 
 @WebSocket
 public class EchoSocket {
 
-    private Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
+    private Set<Session> sessions = Collections.synchronizedSet(new HashSet<>());
+    private List<Session> sessionsList = Collections.synchronizedList(new ArrayList<>());
+    private Object lock = new Object();
 
     @OnWebSocketConnect
     public void onConnect(Session session) {
         System.out.println("Client connected: " + session.getRemoteAddress().getAddress());
         sessions.add(session);
-
+        sessionsList.add(session);
         Thread thread = new Thread(new WebSocketHandler(session));
         thread.start();
+        synchronized (lock){
+            if(sessionsList.size() >=2){
+                Thread matchThread = new MatchHandler();
+                matchThread.start();
+            }
+        }
+
     }
 
     @OnWebSocketClose
@@ -53,6 +61,7 @@ public class EchoSocket {
                     if(!(sessions.isEmpty())){
                         System.out.println("Finns en session i listan");
                     }
+
                     Thread.sleep(10000);
                 } catch (InterruptedException ie){
                     ie.printStackTrace();;
@@ -61,10 +70,27 @@ public class EchoSocket {
         }
     }
 
-    private class MatchHandler{
+    private class MatchHandler extends Thread{
 
-        public MatchHandler(){
-            sessions.clear();
+        private List<Session> matchThread =  Collections.synchronizedList(new ArrayList<>());
+
+        public void run(){
+
+                if (sessionsList.size() >= 2) {
+                    Session session1 = sessionsList.get(0);
+                    Session session2 = sessionsList.get(1);
+
+                    try {
+                        session1.getRemote().sendString("Match found! You are matched with another player.");
+                        session2.getRemote().sendString("Match found! You are matched with another player.");
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    sessionsList.remove(0);
+                    sessionsList.remove(0);
+                }
+
         }
+
     }
 }
