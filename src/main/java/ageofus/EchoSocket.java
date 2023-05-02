664746d2818dfca077586d0c5fb92b8a0aa8f24b
Oliver Bengtsson <58479844+LongeMan@users.noreply.github.com>
2023-05-02 22:04:06 +0200

    New code for sessions
    Ive added a new code to figure out how to get new sessions to be implemented into the list

--- src/main/java/ageofus/EchoSocket.java
+++ src/main/java/ageofus/EchoSocket.java
@@ -8,41 +8,35 @@ import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 @WebSocket
 public class EchoSocket {
 
-    private Set<Session> sessions = Collections.synchronizedSet(new HashSet<>());
-    private List<Session> sessionsList = Collections.synchronizedList(new ArrayList<>());
-    private Object lock = new Object();
-    private volatile int counter = 0;
+
+    private static Set<Session> sessions = new CopyOnWriteArraySet<>();
+
+
+
+
+
 
     @OnWebSocketConnect
     public void onConnect(Session session) {
         System.out.println("Client connected: " + session.getRemoteAddress().getAddress());
         sessions.add(session);
-        sessionsList.add(session);
-
-        Thread thread = new Thread(new WebSocketHandler(session));
-        thread.start();
-        synchronized (lock){
-
-            System.out.println(counter);
-            if(sessionsList.size() >=2){
-                Thread matchThread = new MatchHandler();
-                matchThread.start();
-            }
-        }
+        System.out.println("Amount of connections: "+ sessions.size());
+        // ---- Starting thread -----
+        WebSocketHandler webSocketHandler = new WebSocketHandler(session);
+        webSocketHandler.start();
 
     }
+
 
     @OnWebSocketClose
     public void onClose(Session session, int statusCode, String reason) {
         System.out.println("Connection closed with statusCode=" + statusCode + ", reason=" + reason);
-        sessions.remove(session); // todo ta bara bort specifica sessionen inte alla.
-        if(!(sessions.contains(session))){
-            System.out.println("du tömde skiten");
-        }
+
     }
 
     @OnWebSocketMessage
@@ -59,22 +53,7 @@ public class EchoSocket {
         }
 
         public void run(){
-            while(session.isOpen()){
-                try{
-                    synchronized (lock) {
-                        counter++;
-                    }
-                    System.out.println("Ny tråd skapad");
-                    if(!(sessions.isEmpty())){
-                        System.out.println("Finns en session i listan");
-                        System.out.println(sessionsList.size());
-                    }
-
-                    Thread.sleep(10000);
-                } catch (InterruptedException ie){
-                    ie.printStackTrace();;
-                }
-            }
+            
         }
     }
 
@@ -85,19 +64,7 @@ public class EchoSocket {
         public void run(){
 
 
-                if (sessionsList.size() >= 2) {
-                    Session session1 = sessionsList.get(0);
-                    Session session2 = sessionsList.get(1);
-                    System.out.println("Är denna startad? ");
-                    try {
-                        session1.getRemote().sendString("Match found! You are matched with another player.");
-                        session2.getRemote().sendString("Match found! You are matched with another player.");
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    sessionsList.remove(0);
-                    sessionsList.remove(0);
-                }
+
 
         }
 
