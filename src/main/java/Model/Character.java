929811e84f6dca605c06f33d39591792daaa37e5
jankzzz <fredriklabani@gmail.com>
2023-05-03 14:43:17 +0200

    hej

--- src/main/java/Model/Character.java
+++ src/main/java/Model/Character.java
@@ -14,21 +14,39 @@ public abstract class Character {
         this.isAlive = isAlive;
     }
 
+    /**
+     * Method for each character played to attack
+     */
     public void attack(){}
 
+    /**
+     * Method for each character played to take damage
+     */
     public void takeDamage(){}
 
+    /**
+     * Method to check if each character played is alive or not
+     */
     public void isAlive(){
     }
 
+    /**
+     * Method to get reward for each character type killed
+     */
     public int getKillReward(){
         return killedReward;
     }
 
+    /**
+     * Method to get each character played health
+     */
     public int getHealthPoints() {
         return healthPoints;
     }
 
+    /**
+     * Method to get position of each played character. 
+     */
     public int getPosition() {
         return position;
     }
--- src/main/java/Model/GameManager.java
+++ src/main/java/Model/GameManager.java
@@ -6,21 +6,54 @@ public class GameManager {
 
     private boolean gameWinner;
 
+    /**
+     * Starts the game when two clients are ready
+     */
+    public void startGame(){
+
+    }
+
+    /**
+     * Ends game when client disconects or the base of one client/player is destoryed.
+     */
+    public void endGame(){
+
+    }
+
+
     public void updateGameState(){
         //få info från gameState i matchtråden
 
     }
 
+    /**
+     * Gets position of character from gameThread
+     */
     public void getPosition(){
 
     }
 
+    /**
+     * Gets health of base and if base is destoryed or not
+     */
     public void getBaseStatus(){
 
     }
 
-    public boolean checkGold(){ //Checks so a player has sufficent amount of gold before buying a character
-        if(player.getMoney() >= character)
-        return true;
+    /**
+     * Checks so a player has sufficent amount of gold before buying a character
+     * @return returns if player can buy character or not
+     */
+    public boolean checkGold(){
+        return player.getMoney() >= character;
     }
+
+    /**
+     * Sends gold for characters killed to the player that killed them.
+     */
+    public void getReward(){
+
+    }
+
+
 }
