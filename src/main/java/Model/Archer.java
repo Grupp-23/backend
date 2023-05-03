13afa3cc2b4dc31800bc918ede476fb07c7aeb78
Ludvig Larsson <ludvig@senvestro.se>
2023-05-03 14:40:12 +0200

    Added Javadoc to the 3 different character types classes.

--- src/main/java/Model/Archer.java
+++ src/main/java/Model/Archer.java
@@ -1,5 +1,10 @@
 package Model;
 
+
+/**
+ * Archer class is a template of which attributes an Archer character would have.
+ * It also has methods for different behaviour/ what the Archer can do.
+ */
 public class Archer extends Character {
 
     private final int killedReward = 15; // When an archer is killed the opponent gets 15 gold.
@@ -7,24 +12,47 @@ public class Archer extends Character {
     private final int damage = 30; // Each hit does 30 damage to opponent character.
     private final int attackRange = 50; // Assumed 50 pixels range
 
+    /**
+     * Instantiates the health, position (where on the map the character is) and
+     * if it is alive or not.
+     * @param healthPoints the character's health.
+     * @param position Where it is located.
+     * @param isAlive if it is alive or not.
+     */
     public Archer(int healthPoints, int position, boolean isAlive) {
         super(healthPoints, position, isAlive);
     }
 
+
+    /**
+     * Attack method for attacking an opponent.
+     */
     @Override
     public void attack(){
 
     }
 
+    /**
+     * Method for updating the character's health,
+     * how much damage is based on the opponent.
+     */
     @Override
     public void takeDamage(){
 
     }
 
+
+    /**
+     * Check if the character is alive or not.
+     */
     @Override
     public void isAlive(){
     }
 
+    /**
+     * Sends the reward to the opponent for killing the character.
+     * @return returns an int (gold) to the opponent as a reward.
+     */
     @Override
     public int getKillReward(){
        return killedReward;
--- src/main/java/Model/Melee.java
+++ src/main/java/Model/Melee.java
@@ -1,5 +1,10 @@
 package Model;
 
+
+/**
+ * Melee class is a template of which attributes a Melee character would have.
+ * It also has methods for different behaviour/ what the Melee character can do.
+ */
 public class Melee extends Character {
 
     private final int killedReward = 5; // When an archer is killed the opponent gets 5 gold.
@@ -7,25 +12,45 @@ public class Melee extends Character {
     private final int damage = 20; // Each hit does 20 damage to opponent character.
     private final int attackRange = 10; // Assumed 10 pixels range
 
+    /**
+     * Instantiates the health, position (where on the map the character is) and
+     * if it is alive or not.
+     * @param healthPoints the character's health.
+     * @param position Where it is located.
+     * @param isAlive if it is alive or not.
+     */
     public Melee(int healthPoints, int position, boolean isAlive) {
         super(healthPoints, position, isAlive);
     }
 
+    /**
+     * Attack method for attacking an opponent.
+     */
     @Override
     public void attack(){
 
     }
 
+    /**
+     * Method for updating the character's health,
+     * how much damage is based on the opponent.
+     */
     @Override
     public void takeDamage(){
 
     }
 
+    /**
+     * Check if the character is alive or not.
+     */
     @Override
     public void isAlive(){
-
     }
 
+    /**
+     * Sends the reward to the opponent for killing the character.
+     * @return returns an int (gold) to the opponent as a reward.
+     */
     @Override
     public int getKillReward(){
         return killedReward;
